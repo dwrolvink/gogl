83df02fae5de29a310d35e2412aa7e357f36fcb2
Dorus Rolvink <dwrolvink@gmail.com>
2021-04-07 03:52:46 +0200

    types refactor - comments

--- types.go
+++ types.go
@@ -1,5 +1,18 @@
 package gogl
 
+/* 	TYPES
+
+	General types will be put here. When a type is closely linked to a separate 
+	code file, the type will be put there. 
+		E.g.: the ShaderFileInfo struct is used exclusively to track changes in the 
+		shader files, so that type is put in hotloading.go.
+	Use project level search if you can't find a certain type! 
+
+	The uint32 recastings are mostly to create a type-awareness/-safety in the code, 
+	this convention can be stripped entirely from the code, and it would all still work
+	as intended (if one is thorough).
+*/
+
 type Program struct {
 	ID ProgramID
 	ProgramName string
